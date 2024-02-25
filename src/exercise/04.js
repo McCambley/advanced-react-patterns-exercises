1df781f7cc480b80372fd5eae59b2bc34016108c
McCambley <jakemccamb16@gmail.com>
2024-02-25 16:26:02 -0500

    Add getTogglerProps function to useToggle hook

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,20 +7,34 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
+  const getTogglerProps = ({on}) => {
+    return {
+      onClick: toggle,
+      'aria-pressed': on,
+      on,
+    }
+  }
 
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  return {on, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
+  console.log({getTogglerProps, on})
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
